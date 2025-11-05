8067f1f9f0ffc21cc26eaea237bd4cb5fd9b4e13
zach <zekariyasamdu24@gmail.com>
2025-11-05 15:26:25 -0600

    chore: setting up a theme and shadcn components

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import type React from "react"
+import { AppSidebar } from "~/components/app-sidebar"
+import { SidebarProvider } from "~/components/ui/sidebar"
+
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className=" w-full">
+        {children}
+      </main>
+    </SidebarProvider>
+  )
+}
