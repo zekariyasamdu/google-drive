b5c382509a651b37ff0809a030f15e34358a4bab
zach <zekariyasamdu24@gmail.com>
2026-01-25 18:12:44 -0600

    fix

--- src/lib/types/db.ts
+++ src/lib/types/db.ts
@@ -1,11 +1,13 @@
 import type { InferSelectModel, InferInsertModel  } from "drizzle-orm";
-import type { filesSchema, foldersSchema } from "~/server/db/schema";
+import type { filesSchema, foldersSchema, user } from "~/server/db/schema";
 
 export type TFileSelect = InferSelectModel<typeof filesSchema>;
 export type TFolderSelect = InferSelectModel<typeof foldersSchema>;
 
 export type TFileInsert = InferInsertModel<typeof filesSchema>;
 export type TFolderInsert = InferInsertModel<typeof foldersSchema>;
+
+export type TUserInsert = InferInsertModel<typeof user>;
 
 // File Guard
 export function isFile(item: TFolderSelect | TFileSelect): item is TFileSelect {
